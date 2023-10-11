17a49577cdcff7c26ff6d8966176e6d18610a753
Aashu Tiwari <ashutripathi573@gmail.com>
2023-10-11 12:29:21 +0530

    Portfolio

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import '../styles/globals.css';
-
+import Layout from '../components/Layout';
+import { AnimatePresence,motion } from 'framer-motion';
+import  Transition  from '../components/Transition';
+import { useRouter } from 'next/router';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter()
+  return(
+    <Layout>
+        <AnimatePresence>
+          <motion.div key={router.route} className='h-full'>
+                <Transition/>
+                <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+    </Layout>
+
+  )
 }
 
 export default MyApp;
